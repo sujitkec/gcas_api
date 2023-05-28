5f9628a931cbc9010a9f370389cefc1340ded8a3
Sujit Balasubramanian <sujitbalasubramanianr@gmail.com>
2023-05-28 10:28:29 +0530

    cors

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ const generalRoutes = require('./routes/general')
 const adminRoutes = require('./routes/admin')
 
 const app = express();
-app.use(express.json());
-app.use(bodyparser.urlencoded({ extended: false }));
+app.use(cors({
+    origin: "https://idotdevelop.github.io/",
+}));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "*")
     res.header(
@@ -21,6 +22,8 @@ app.use((req, res, next) => {
     )
     next();
 })
+app.use(express.json());
+app.use(bodyparser.urlencoded({extended: false}));
 
 mongoose.connect(process.env.DB).then(() => {
     console.log('Db connection open')
